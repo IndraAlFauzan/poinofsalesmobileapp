bcdfdbf5d4fa05b27fe19f6c92c369e9cbcbeebd
IndraAlFauzan <nurulindra35@gmail.com>
2025-08-11 13:30:08 +0700

    Update NDK version and add google_fonts dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "indraalfauzan.id.posmobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
